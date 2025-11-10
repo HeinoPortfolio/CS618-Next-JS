4064cb7e3fecae3af0fda6d3c25b9a0d713e66b0
HeinoPorfolio <meheino@sbcglobal.net>
2025-11-10 10:53:50 -0500

    chore: created basic static structure for application

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,16 +2,22 @@
     Do not need to  provide a helmut.  Only need to provide the meta data
 */
 
+import { Navigation } from '@/components/Navigation'
+
 export const metadata = {
   title: 'Full-Stack Next.js Blog',
   description: 'A blog about React and Next.js',
 }
 
-// Root layout for the application ============================================
 export default function RootLayout({ children }) {
+  const user = { username: 'matt' }
   return (
     <html lang='en'>
       <body>
+        <nav>
+          <Navigation username={user?.username} />
+        </nav>
+        <br />
         <main>{children}</main>
       </body>
     </html>
