3fae546e152fd9c87e6b2c302a581deda971286a
HeinoPorfolio <meheino@sbcglobal.net>
2025-11-11 08:18:42 -0500

    chore: implemented optimization of fonts and images

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,30 @@ import { cookies } from 'next/headers'
 import { getUserInfoByToken } from '@/data/users'
 import { Navigation } from '@/components/Navigation'
 
+import Image from 'next/image'
+import logo from './logo.png'
+
+// Import the font from Google ================================
+import { Inter } from 'next/font/google'
+
+// Import a fun font ==========================================
+//import { Are_You_Serious } from 'next/font/google'
+
+// Font variable ======
+/*
+const serious = Are_You_Serious({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+})
+*/
+
+// Another type of font ===============================
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 export const metadata = {
   title: 'Full-Stack Next.js Blog',
   description: 'A blog about React and Next.js',
@@ -23,8 +47,14 @@ export default async function RootLayout({ children }) {
   const user = await getUserInfoByToken(token?.value)
 
   return (
-    <html lang='en'>
+    <html lang='en' className={inter.className}>
       <body>
+        <Image
+          src={logo}
+          alt='Full-Stack Next.js Blog Logo'
+          width={500}
+          height={50}
+        />
         <nav>
           <Navigation username={user?.username} logoutAction={logoutAction} />
         </nav>
